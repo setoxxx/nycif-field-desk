56b66ebde0c8d6047d6f6882cb9660112bc04eff
setoxxx <setoxxx@gmail.com>
2026-06-27 10:42:22 -0400

    Bump cache for v0.7.2 today date fix

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'nycif-v071-lite-date-scroll';
-const APP_SHELL = ['./','./index.html','./style.css','./fielddesk-v02.css','./weekstrip-v06-safe.css','./date-normalizer-v071-safe.js','./app-v06-safe.js','./stats-v05-safe.js','./manifest.json','./icons/icon-192.svg','./icons/icon-512.svg'];
+const CACHE_NAME = 'nycif-v072-today-local-date';
+const APP_SHELL = ['./','./index.html','./style.css','./fielddesk-v02.css','./weekstrip-v06-safe.css','./date-normalizer-v072-safe.js','./app-v06-safe.js','./stats-v05-safe.js','./manifest.json','./icons/icon-192.svg','./icons/icon-512.svg'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))); self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))); self.clients.claim(); });
 self.addEventListener('fetch', event => {
