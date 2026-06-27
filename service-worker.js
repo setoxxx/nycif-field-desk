c2329b6a2030e4318226556ce4ab0c4b61d3186d
setoxxx <setoxxx@gmail.com>
2026-06-27 10:32:36 -0400

    Bump cache for date normalizer

--- service-worker.js
+++ service-worker.js
@@ -1,59 +1,10 @@
-const CACHE_NAME = 'nycif-field-desk-v062-full-data-test';
-
-const APP_SHELL = [
-  './',
-  './index.html',
-  './style.css',
-  './fielddesk-v02.css',
-  './weekstrip-v06-safe.css',
-  './weekstrip-v06-fix.js',
-  './app-v06-safe.js',
-  './app-v03.js',
-  './stats-v05-safe.js',
-  './manifest.json',
-  './icons/icon-192.svg',
-  './icons/icon-512.svg'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
-    )
-  );
-  self.clients.claim();
-});
-
+const CACHE_NAME = 'nycif-v07-date-normalizer';
+const APP_SHELL = ['./','./index.html','./style.css','./fielddesk-v02.css','./weekstrip-v06-safe.css','./weekstrip-v06-fix.js','./date-normalizer-v07-safe.js','./app-v06-safe.js','./stats-v05-safe.js','./manifest.json','./icons/icon-192.svg','./icons/icon-512.svg'];
+self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))); self.skipWaiting(); });
+self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))); self.clients.claim(); });
 self.addEventListener('fetch', event => {
   const url = new URL(event.request.url);
-
-  if (url.origin === location.origin) {
-    event.respondWith(
-      fetch(event.request)
-        .then(response => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-          return response;
-        })
-        .catch(() => caches.match(event.request))
-    );
-    return;
-  }
-
-  if (url.hostname === 'raw.githubusercontent.com') {
-    event.respondWith(
-      fetch(event.request)
-        .then(response => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-          return response;
-        })
-        .catch(() => caches.match(event.request))
-    );
+  if (url.origin === location.origin || url.hostname === 'raw.githubusercontent.com') {
+    event.respondWith(fetch(event.request).then(response => { const copy = response.clone(); caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)); return response; }).catch(() => caches.match(event.request)));
   }
 });
